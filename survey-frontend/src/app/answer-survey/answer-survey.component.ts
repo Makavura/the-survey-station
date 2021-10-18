9a8ac0a1b9d1284318193d76fe499aed35963296
Makavura <makavuramughanga@gmail.com>
2021-10-18 21:36:39 +0300

    feature: render specific survey and its questions; include contact details form and back to surveys button

--- survey-frontend/src/app/answer-survey/answer-survey.component.ts
+++ survey-frontend/src/app/answer-survey/answer-survey.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { SurveysService } from '../services/surveys.service';
-
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 @Component({
   selector: 'app-answer-survey',
   templateUrl: './answer-survey.component.html',
@@ -8,12 +13,27 @@ import { SurveysService } from '../services/surveys.service';
 })
 export class AnswerSurveyComponent implements OnInit {
   surveys: any = [];
+  activeSurveyId!: any;
+  activeSurvey!: any;
+  respondingToSurvey!: boolean;
+  form = new FormGroup({});
+  model!: any;
+  fields: FormlyFieldConfig[] = [ ];
+
   constructor(
     private surveysService: SurveysService,
-  ) { }
+    private fb: FormBuilder,
+  ) { 
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
     this.loadSurveys()
+    
   }
 
   loadSurveys() {
@@ -23,5 +43,30 @@ export class AnswerSurveyComponent implements OnInit {
         console.log(this.surveys)
         return this.surveys
       }, (err) => { console.warn(err) });
+  }
+
+  respondToSurvey(surveyId: any) {
+    this.activeSurveyId = surveyId;
+    this.surveys.forEach((survey: any) => {
+      if (survey["_id"] == surveyId) {
+        this.activeSurvey = survey
+        this.fields = this.activeSurvey["questions"]
+        this.respondingToSurvey = true;
+        this.activeSurvey && this.respondingToSurvey;
+      }
+    });
+  }
+
+  backToSurveys(){
+    this.respondingToSurvey = false;
+    return this.respondingToSurvey;
+  }
+
+  onSubmitSurvey(){ 
+    console.log(this.form.value)
+    console.log(Object.keys(this.form.value))
+    Object.keys(this.form.value).forEach((key) => {
+
+    })
   }
 }
