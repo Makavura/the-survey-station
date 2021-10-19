1f7f5f9ad04cebf60c8a0e6e7e030a41ad4cd432
Makavura <makavuramughanga@gmail.com>
2021-10-19 07:19:37 +0300

    --allow-empty

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     "origin": ["*", "http://localhost:4200", "https://rgsdfgdfgsdfgdfhdhe3423.web.app"],
-    "allowedHeaders": ['Access-Control-Allow-Origin', '*'],
+    "allowedHeaders": ['Access-Control-Allow-Origin', 'https://rgsdfgdfgsdfgdfhdhe3423.web.app'],
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
   await app.listen(3000, () =>{
